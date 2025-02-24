5f7f26dba6d97eb389104f6ac55783417a537929
Hassan-301 <124438591+Hassan-301@users.noreply.github.com>
2025-02-24 22:17:41 +0500

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  //baseURL: "http://localhost:5000/api",
+    baseURL: "http://blog-backend-sandy.vercel.app/",
 })
 
 instance.interceptors.request.use((config) => {
